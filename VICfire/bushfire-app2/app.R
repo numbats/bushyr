135031e97f26174455029281f018080b078a7ec7
Brenwin <ang0005@student.monash.edu>
2021-11-23 09:58:09 +1100

    include bushfire risk apps (to be combined)

---
+++ VICfire/bushfire-app2/app.R
@@ -0,0 +1,317 @@
+library(shiny)
+library(tidyverse)
+library(tmap)
+library(plotly)
+library(leaflet)
+library(DT)
+library(shinyWidgets)
+library(shinycssloaders)
+
+# **************************************** outside app ****************************************
+
+# ==================== read in data ====================
+load(here::here("data/ida.RData"))
+load(here::here("data/eda.RData"))
+
+# --- set spinner colour to red
+options(spinner.color = "#b22222")
+
+
+# **************************************** define UI ****************************************
+ui <- fluidPage(
+
+    # add app title
+    tags$h1("Bushfire Risk Information",
+            style = "font-family: Impact; color: #1e90ff; font-size: 60px",
+            align = "center"),
+    br(),
+
+    sidebarLayout(
+        # === column for user interactivity
+        sidebarPanel(width = 2,
+            # --- user; `month` checkbox buttons
+            shinyWidgets::checkboxGroupButtons(inputId = "month_selected",
+                                               label = "Month(s)",
+                                               choices = c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar"),
+                                               status = "danger",
+                                               direction = "vertical",
+                                               size = "lg") # large size
+        ),
+
+        # === column for leaflet map output
+        mainPanel(width = 10,
+            leaflet::leafletOutput("map",
+                                   height = 500) %>%
+                shinycssloaders::withSpinner()
+        )
+    ),
+        br(), br(),
+
+    # ========== DT::datatable ==========
+    fluidRow(
+        DT::DTOutput("datatable") %>%
+            shinycssloaders::withSpinner()
+    ),
+    br(), br(),
+
+
+
+    # ========== plotly ==========
+    fluidRow(
+        # === column for `fire_count` vs. `bf_season` bar plotly
+        column(width = 4,
+               plotly::plotlyOutput("fire_bf_season_plotly") %>%
+                   shinycssloaders::withSpinner()
+               ),
+
+        column(width = 4,
+               plotly::plotlyOutput("fire_month_plotly") %>%
+                   shinycssloaders::withSpinner()
+               ),
+
+        column(width = 4,
+               plotly::plotlyOutput("max_temp_plotly") %>%
+                   shinycssloaders::withSpinner())
+    )
+
+)
+
+# **************************************** define server ****************************************
+server <- function(input, output) {
+
+    # ===== create leaflet map =====
+    output$map <- leaflet::renderLeaflet({
+
+        # --- create base map
+        base_map <- leaflet(options = leafletOptions(
+            # set min & max zoom
+            minZoom = 6.45,
+            maxZoom = 11
+        )) %>%
+            # add provider Tiles
+            addProviderTiles(provider = "OpenStreetMap",
+                             group = "OSM") %>%
+            addProviderTiles(provider = "CartoDB",
+                             group = "Carto") %>%
+            # set map to within Victoria bounding box
+            leaflet::fitBounds(lng1 = 140.9617,
+                               lng2 = 149.9763,
+                               lat1 = -39.13396,
+                               lat2 = -33.99605) %>%
+            # set max bounds of map
+            # disable dragging map; too far out of bounds
+            setMaxBounds(lng1 = 140.9617 - 0.4,
+                         lng2 = 149.9763 + 0.4,
+                         lat1 = -39.13396 - 0.4,
+                         lat2 = -33.99605 + 0.4)
+
+        # --- palette for polygon data
+        pal <- leaflet::colorNumeric(palette = "YlOrRd",
+                                     domain = c(1:50), # scale
+                                     reverse = F)
+
+        # --- create df for each bushfire season for circle marker groups
+        bf_season_2016_2017_df <- cluster_16_21_sf %>%
+            filter(bf_season == "2016-2017")
+
+        bf_season_2017_2018_df <- cluster_16_21_sf %>%
+            filter(bf_season == "2017-2018")
+
+        bf_season_2018_2019_df <- cluster_16_21_sf %>%
+            filter(bf_season == "2018-2019")
+
+        bf_season_2019_2020_df <- cluster_16_21_sf %>%
+            filter(bf_season == "2019-2020")
+
+        bf_season_2020_2021_df <- cluster_16_21_sf %>%
+            filter(bf_season == "2020-2021")
+
+
+        base_map %>%
+            # add outline of Victoria
+            addPolylines(data = vic_map_sf,
+                         color = "black",
+                         weight = 3) %>%
+
+            # add gridded cell
+            addPolygons(data = ignition_rasterize_cluster_bf_season %>% filter(bf_season == "2019-2020"),
+                        opacity = 0.1,
+                        layerId = ~id,
+                        fillOpacity = 0.4,
+                        color = ~pal(fire_count), # fill by `fire_count`; using above palette
+                        popup = ~paste0("<b> id: ", id, "</b>", "<br/>",
+                                        "number of ignitions: ", fire_count)) %>% # click pop-up texts
+
+            # --- add circle markers; each `bf_season` as a layer
+            # 2016-2017
+            addCircleMarkers(data = bf_season_2016_2017_df,
+                             radius = 2,
+                             fillOpacity = 0.01,
+                             color = "grey",
+                             group = "2016-2017") %>%
+            # 2017-2018
+            addCircleMarkers(data = bf_season_2017_2018_df,
+                             radius = 2,
+                             fillOpacity = 0.01,
+                             color = "grey",
+                             group = "2017-2018") %>%
+            # 2018-2019
+            addCircleMarkers(data = bf_season_2018_2019_df,
+                             radius = 2,
+                             fillOpacity = 0.01,
+                             color = "grey",
+                             group = "2018-2019") %>%
+            # 2019-2020
+            addCircleMarkers(data = bf_season_2019_2020_df,
+                             radius = 2,
+                             fillOpacity = 0.01,
+                             color = "grey",
+                             group = "2019-2020") %>%
+            # 2020-2021
+            addCircleMarkers(data = bf_season_2018_2019_df,
+                             radius = 2,
+                             fillOpacity = 0.01,
+                             color = "grey",
+                             group = "2020-2021") %>%
+
+            # add user controls; for overlay groups
+            addLayersControl(baseGroups = c("OSM", "Carto"),
+                             overlayGroups = c("2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021"),
+                             options = layersControlOptions(collapsed = FALSE)) %>%
+
+            # uncheck groups
+            hideGroup(c("2017-2018", "2018-2019", "2019-2020", "2020-2021")) %>%
+
+            # add & customise legend
+            addLegend(pal = pal,
+                      values = c(1:50),
+                      title = "ignition counts",
+                      position = "bottomright") %>%
+
+            # add Reset button
+            leaflet.extras::addResetMapButton()
+
+    })
+
+    # --- df based on user click
+    model_df_id <- shiny::eventReactive(input$map_shape_click, { # invalidated; each time; user click grid cell
+        # based on user map click
+        model_df3 %>%
+            filter(id == input$map_shape_click$id)
+    })
+
+    # --- DT::datatable; based on cell clicked
+    output$datatable <- DT::renderDT(
+        model_df_id() %>%
+            DT::datatable(options = list(scrollX = T,
+                                         pageLength = 5)) %>%
+            DT::formatRound(columns = 6:33,
+                            digits = 3)
+    )
+
+    # --- plot `fire_count` vs. `bf_season`; bar plotly
+    output$fire_bf_season_plotly <- plotly::renderPlotly({
+        model_df_id() %>%
+            # sum fires over bushfire season
+            group_by(bf_season) %>%
+            summarise(fire_count = sum(fire_count)) %>%
+            # plot_ly
+            plotly::plot_ly(x = ~bf_season,
+                            y = ~fire_count,
+                            type = "bar",
+                            text = ~fire_count,
+                            textposition = "auto") %>%
+            plotly::layout(
+                # plot title
+                title = "number of fires ignitions in bushfire season",
+
+                # x-axis
+                xaxis = list(type = "category", # categorical variable
+                             title = "bushfire season"), # axis title
+
+                # y-axis
+                yaxis = list(title = "number of fire ignitions",
+                             type = "numeric",
+                             range = c(0, 40))
+            )
+    })
+
+
+    # --- plot `fire_count` vs. `month`; bar plotly
+    output$fire_month_plotly <- plotly::renderPlotly({
+        model_df_id() %>%
+            group_by(bf_season,
+                     month) %>%
+            summarise(fire_count = sum(fire_count)) %>%
+            # wide form (each `month` have own column)
+            pivot_wider(names_from = month,
+                        values_from = fire_count) %>%
+            # plot
+            plot_ly() %>%
+            # add bar for each month
+            add_trace(x = ~bf_season,
+                      y = ~`10`,
+                      name = "Oct",
+                      text = ~`10`,
+                      textposition = "outside") %>%
+            add_trace(x = ~bf_season,
+                      y = ~`11`,
+                      name = "Nov",
+                      text = ~`11`,
+                      textposition = "outside") %>%
+            add_trace(x = ~bf_season,
+                      y = ~`12`,
+                      name = "Dec",
+                      text = ~`12`,
+                      textposition = "outside") %>%
+            add_trace(x = ~bf_season,
+                      y = ~`1`,
+                      name = "Jan",
+                      text = ~`1`,
+                      textposition = "outside") %>%
+            add_trace(x = ~bf_season,
+                      y = ~`2`,
+                      name = "Feb",
+                      text = ~`2`,
+                      textposition = "outside") %>%
+            add_trace(x = ~bf_season,
+                      y = ~`3`,
+                      name = "Mar",
+                      text = ~`3`,
+                      textposition = "outside") %>%
+            plotly::layout(barmode = "group",
+                           title = "fire ignitions against months in bushfire season",
+                           yaxis = list(title = "number of ignitions",
+                                        type = "numeric",
+                                        range = c(0, 40)),
+                           xaxis = list(type = "category",
+                                        title = "bushfire season | months"),
+                           bargap = 0.05,
+                           bargroupgap = 0.25)
+    })
+
+    # --- plot
+    output$max_temp_plotly <- plotly::renderPlotly({
+        model_df_id() %>%
+            # extract date
+            mutate(date = paste(year, month, sep = "") %>% lubridate::ym(),
+                   .after = "month") %>%
+            plot_ly(x = ~date,
+                    y = ~max_temp,
+                    split = ~bf_season,
+                    type = "scatter",
+                    mode = "lines+markers") %>%
+            plotly::layout(title = "max temperature over bushfire season", # title
+                           # set tick labels
+                           xaxis = list(tickformat = "%b\n%Y",
+                                        ticklabelmode = "period",
+                                        dtick = "M12"), # every 12 months
+                           # legend
+                           legend = list(title = list(text = "bushfire_season"))
+            )
+    })
+}
+
+# Run the application
+shinyApp(ui = ui,
+         server = server)
---
+++ VICfire/bushfire-app3/app.R
@@ -0,0 +1,405 @@
+library(shiny)
+library(tidyverse)
+library(tmap)
+library(plotly)
+library(leaflet)
+library(DT)
+library(shinyWidgets)
+library(shinycssloaders)
+library(ranger)
+
+# **************************************** outside app ****************************************
+
+ignition_rasterize_cluster_bf_season <- ignition_rasterize_cluster_bf_season %>%
+  mutate(id = as_factor(id))
+
+
+# ==================== read in data ====================
+load(here::here("data/ida.RData"))
+load(here::here("data/eda.RData"))
+model <- readRDS(here::here("data/rfmodel_final.rds"))
+
+# --- set spinner colour to red
+options(spinner.color = "#b22222")
+
+
+
+# **************************************** define UI ****************************************
+ui <- fluidPage(
+
+  shinyWidgets::setBackgroundColor(color = "ghostwhite"),
+
+  # add app title
+  tags$h1("Bushfire Risk Predictions",
+          style = "font-family: Impact; color: #1e90ff; font-size: 60px",
+          align = "center"),
+  br(),
+
+  sidebarLayout(
+    # === column for user interactivity
+    sidebarPanel(width = 2,
+
+                 # --- user; choose month
+                 radioGroupButtons(
+                   inputId = "month_chosen",
+                   label = "Choose month",
+                   choiceNames = c("Oct", "Nov", "Dec", "Jan", "Feb", "Mar"),
+                   choiceValues = c(10, 11, 12, 1, 2, 3),
+                   selected = 12,
+                   status = "danger"
+                 ),
+
+                 # --- user; toggle `max_temp`
+                 shinyWidgets::sliderTextInput(inputId = "max_temp_slider",
+                                               label = "toggle max temperature",
+                                               choices = 1:100)
+    ),
+
+    # === column for leaflet map output
+    mainPanel(width = 10,
+              leaflet::leafletOutput("map",
+                                     height = 500) %>%
+                shinycssloaders::withSpinner()
+    )
+  ),
+  br(), br(),
+
+  # ========== DT::datatable ==========
+  fluidRow(
+    DT::DTOutput("datatable") %>%
+      shinycssloaders::withSpinner()
+  ),
+  br(), br(),
+
+
+
+  # ========== plotly ==========
+  fluidRow(
+    # === column for `fire_count` vs. `bf_season` bar plotly
+    column(width = 4,
+           plotly::plotlyOutput("fire_bf_season_plotly") %>%
+             shinycssloaders::withSpinner()
+    ),
+
+    column(width = 4,
+           plotly::plotlyOutput("fire_month_plotly") %>%
+             shinycssloaders::withSpinner()
+    ),
+
+    column(width = 4,
+           plotly::plotlyOutput("max_temp_plotly") %>%
+             shinycssloaders::withSpinner())
+  )
+
+)
+
+# **************************************** define server ****************************************
+server <- function(input, output) {
+
+  # --- based on user selected month; create reactive dfs for each bushfire season for circle marker overlay groups
+  cluster_16_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2016,
+             month == input$month_chosen)
+  })
+
+  cluster_17_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2017,
+             month == input$month_chosen)
+  })
+
+  cluster_18_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2018,
+             month == input$month_chosen)
+  })
+
+  cluster_19_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2019,
+             month == input$month_chosen)
+  })
+
+  cluster_20_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2020,
+             month == input$month_chosen)
+  })
+
+  cluster_21_sf <- shiny::reactive({
+    cluster_16_21_sf %>%
+      filter(year == 2021,
+             month == input$month_chosen)
+  })
+
+  # --- based on user selected month; predict avg
+  model_df_pred <- shiny::reactive({
+
+    model_df_pred_temp <- model_df2 %>%
+      na.omit() %>%
+      ungroup() %>%
+      # allow users: choose month
+      dplyr::filter(month == input$month_chosen) %>% # filter to user selected month
+      # make predictions
+      mutate(pred = predict(model, .)$predictions,
+             .after = "fire_count") %>%
+      group_by(id) %>%
+      summarise(avg_pred = mean(pred)) %>%
+      ungroup() %>%
+      mutate(total = sum(avg_pred)) %>%
+      mutate(avg_pred_prop = avg_pred / total,
+             .after = "avg_pred")
+
+    # extract `id` & `geometry` column (i.e. polygon for each grid cell)
+    vic_raster_crop_sf %>%
+      mutate(id = as_factor(id)) %>%
+      # join with predicted data
+      left_join(., model_df_pred_temp)
+  })
+
+
+
+  # ===== create leaflet map =====
+
+  # --- palette intervals; based on user selected month
+  pal_intervals <- shiny::reactive({
+    classInt::classIntervals(model_df_pred()$avg_pred_prop,
+                             n = 5,
+                             style = "pretty")
+  })
+
+  output$map <- leaflet::renderLeaflet({
+
+    # ---  palette create
+    pal <- leaflet::colorBin(palette = "YlOrRd",
+                             domain = pal_intervals()$brks, # palette intervals; based on user selected month
+                             bins = 5,
+                             pretty = T,
+                             reverse = F)
+
+    # --- create base map
+    base_map <- leaflet(options = leafletOptions(
+      # set min & max zoom
+      minZoom = 6.45,
+      maxZoom = 11
+    )) %>%
+      # add provider Tiles
+      addProviderTiles(provider = "OpenStreetMap",
+                       group = "OSM") %>%
+      addProviderTiles(provider = "CartoDB",
+                       group = "Carto") %>%
+      # set map to within Victoria bounding box
+      leaflet::fitBounds(lng1 = 140.9617,
+                         lng2 = 149.9763,
+                         lat1 = -39.13396,
+                         lat2 = -33.99605) %>%
+      # set max bounds of map
+      # disable dragging map; too far out of bounds
+      setMaxBounds(lng1 = 140.9617 - 0.4,
+                   lng2 = 149.9763 + 0.4,
+                   lat1 = -39.13396 - 0.4,
+                   lat2 = -33.99605 + 0.4)
+
+    # --- palette for polygon data
+    pal <- leaflet::colorBin(palette = "YlOrRd",
+                             domain = pal_intervals()$brks,
+                             bins = 5,
+                             pretty = T,
+                             reverse = F)
+
+
+    base_map %>%
+      # add outline of Victoria
+      addPolylines(data = vic_map_sf,
+                   color = "black",
+                   weight = 3) %>%
+
+      # add gridded cell
+      addPolygons(data = model_df_pred(),
+                  opacity = 0.1,
+                  layerId = ~id,
+                  fillOpacity = 0.4,
+                  color = ~pal(avg_pred_prop), # fill by  avg. predicted proportions; using above palette
+                  popup = ~paste0("<b> id: ", id, "</b>", "<br/>",
+                                  "number of ignitions: ", round(avg_pred_prop, 3))) %>% # click pop-up texts
+
+      # --- add circle markers; each `bf_season` as a layer
+      # 2016
+      addCircleMarkers(data = cluster_16_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2016") %>%
+      # 2017
+      addCircleMarkers(data = cluster_17_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2017") %>%
+      # 2018
+      addCircleMarkers(data = cluster_18_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2018") %>%
+      # 2019
+      addCircleMarkers(data = cluster_19_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2019") %>%
+      # 2020
+      addCircleMarkers(data = cluster_20_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2020") %>%
+      # 2021
+      addCircleMarkers(data = cluster_21_sf(),
+                       radius = 2,
+                       fillOpacity = 0.01,
+                       color = "grey",
+                       group = "2021") %>%
+
+      # add user controls; for overlay groups
+      addLayersControl(baseGroups = c("OSM", "Carto"),
+                       overlayGroups = c("2016", "2017", "2018", "2019", "2020", "2021"),
+                       options = layersControlOptions(collapsed = FALSE)) %>%
+
+      # uncheck groups
+      hideGroup(c("2017", "2018", "2019", "2020", "2021")) %>%
+
+      # add & customise legend
+      addLegend(pal = pal,
+                values = pal_intervals()$brks,
+                title = "predicted avg. proportion",
+                position = "bottomright") %>%
+
+      # add Reset button
+      leaflet.extras::addResetMapButton()
+
+  })
+
+  # --- df based on user click
+  model_df_id <- shiny::eventReactive(input$map_shape_click, { # invalidated; each time; user click grid cell
+    # based on user map click
+    model_df3 %>%
+      filter(id == input$map_shape_click$id)
+  })
+
+  # --- DT::datatable; based on cell clicked
+  output$datatable <- DT::renderDT(
+    model_df_pred() %>%
+      DT::datatable(options = list(scrollX = T,
+                                   pageLength = 5)) %>%
+      DT::formatRound(columns = 2:4,
+                      digits = 3)
+  )
+
+  # --- plot `fire_count` vs. `bf_season`; bar plotly
+  output$fire_bf_season_plotly <- plotly::renderPlotly({
+    model_df_id() %>%
+      # sum fires over bushfire season
+      group_by(bf_season) %>%
+      summarise(fire_count = sum(fire_count)) %>%
+      # plot_ly
+      plotly::plot_ly(x = ~bf_season,
+                      y = ~fire_count,
+                      type = "bar",
+                      text = ~fire_count,
+                      textposition = "auto") %>%
+      plotly::layout(
+        # plot title
+        title = "number of fires ignitions in bushfire season",
+
+        # x-axis
+        xaxis = list(type = "category", # categorical variable
+                     title = "bushfire season"), # axis title
+
+        # y-axis
+        yaxis = list(title = "number of fire ignitions",
+                     type = "numeric",
+                     range = c(0, 40))
+      )
+  })
+
+
+  # --- plot `fire_count` vs. `month`; bar plotly
+  output$fire_month_plotly <- plotly::renderPlotly({
+    model_df_id() %>%
+      group_by(bf_season,
+               month) %>%
+      summarise(fire_count = sum(fire_count)) %>%
+      # wide form (each `month` have own column)
+      pivot_wider(names_from = month,
+                  values_from = fire_count) %>%
+      # plot
+      plot_ly() %>%
+      # add bar for each month
+      add_trace(x = ~bf_season,
+                y = ~`10`,
+                name = "Oct",
+                text = ~`10`,
+                textposition = "outside") %>%
+      add_trace(x = ~bf_season,
+                y = ~`11`,
+                name = "Nov",
+                text = ~`11`,
+                textposition = "outside") %>%
+      add_trace(x = ~bf_season,
+                y = ~`12`,
+                name = "Dec",
+                text = ~`12`,
+                textposition = "outside") %>%
+      add_trace(x = ~bf_season,
+                y = ~`1`,
+                name = "Jan",
+                text = ~`1`,
+                textposition = "outside") %>%
+      add_trace(x = ~bf_season,
+                y = ~`2`,
+                name = "Feb",
+                text = ~`2`,
+                textposition = "outside") %>%
+      add_trace(x = ~bf_season,
+                y = ~`3`,
+                name = "Mar",
+                text = ~`3`,
+                textposition = "outside") %>%
+      plotly::layout(barmode = "group",
+                     title = "fire ignitions against months in bushfire season",
+                     yaxis = list(title = "number of ignitions",
+                                  type = "numeric",
+                                  range = c(0, 40)),
+                     xaxis = list(type = "category",
+                                  title = "bushfire season | months"),
+                     bargap = 0.05,
+                     bargroupgap = 0.25)
+  })
+
+  # --- plot
+  output$max_temp_plotly <- plotly::renderPlotly({
+    model_df_id() %>%
+      # extract date
+      mutate(date = paste(year, month, sep = "") %>% lubridate::ym(),
+             .after = "month") %>%
+      plot_ly(x = ~date,
+              y = ~max_temp,
+              split = ~bf_season,
+              type = "scatter",
+              mode = "lines+markers") %>%
+      plotly::layout(title = "max temperature over bushfire season", # title
+                     # set tick labels
+                     xaxis = list(tickformat = "%b\n%Y",
+                                  ticklabelmode = "period",
+                                  dtick = "M12"), # every 12 months
+                     # legend
+                     legend = list(title = list(text = "bushfire_season"))
+      )
+  })
+}
+
+# Run the application
+shinyApp(ui = ui,
+         server = server)
